3ce287aa231061a2093ec3f13de1b11f7f20d1a0
Fudgey <fudgebucket27@gmail.com>
2022-11-18 13:13:44 +1000

    boiler plate

--- LoopRareAPI/Controllers/Rarity.cs
+++ LoopRareAPI/Controllers/Rarity.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using LoopRareAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Cosmos;
 
@@ -16,12 +17,40 @@ namespace LoopRareAPI.Controllers
 
         public Rarity(IConfiguration config)
         {
+            _config = config;
             _client = new CosmosClient(
                accountEndpoint: _config.GetValue<string>("CosmosDbEndpoint"),
                authKeyOrResourceToken: _config.GetValue<string>("CosmosDbAuthKey"),
                clientOptions: new CosmosClientOptions() { ApplicationName = _config.GetValue<string>("CosmosDbApplicationName") });
             _container = _client.GetContainer(databaseId: _config.GetValue<string>("CosmosDbDatabaseId"), 
                                               containerId: _config.GetValue<string>("CosmosDbContainerId"));
+        }
+
+        [HttpGet]
+        [Route("rankings")]
+        public async Task<Rankings> GetRankings(string collectionName)
+        {
+            try
+            {
+                Rankings rankings = null;
+                var iterator = _container.GetItemQueryIterator<Rankings>($"SELECT * FROM c WHERE c.name = '{collectionName}-Ranks'");
+                while (iterator.HasMoreResults)
+                {
+                    foreach (var item in (await iterator.ReadNextAsync()).Resource)
+                    {
+                        rankings = item;
+                    }
+                }
+                return rankings;
+            }
+            catch (CosmosException ex)
+            {
+                return null;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
         }
 
 
---
+++ LoopRareAPI/Models/Rankings.cs
@@ -0,0 +1,16 @@
+﻿namespace LoopRareAPI.Models
+{
+    public class Rankings
+    {
+        public string name { get; set; }
+        public List<Ranking> rankings { get; set; } = new List<Ranking>();
+    }
+    public class Ranking
+    {
+        public int Id { get; set; }
+        public int Rank { get; set; }
+        public string Tier { get; set; }
+        public int Score { get; set; }
+
+    }
+}
