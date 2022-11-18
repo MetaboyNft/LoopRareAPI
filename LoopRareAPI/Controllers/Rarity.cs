9b8fefb0d8a02eca1707e38573fef9da690d7bb6
Fudgey <fudgebucket27@gmail.com>
2022-11-18 10:22:59 +1000

    setup constructor

---
+++ LoopRareAPI/Controllers/Rarity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
+
+namespace LoopRareAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Rarity : ControllerBase
+    {
+        readonly CosmosClient _client;
+
+        private Container _container;
+
+        private IConfiguration? _config;
+
+        public Rarity(IConfiguration config)
+        {
+            _client = new CosmosClient(
+               accountEndpoint: _config.GetValue<string>("CosmosDbEndpoint"),
+               authKeyOrResourceToken: _config.GetValue<string>("CosmosDbAuthKey"),
+               clientOptions: new CosmosClientOptions() { ApplicationName = _config.GetValue<string>("CosmosDbApplicationName") });
+            _container = _client.GetContainer(databaseId: _config.GetValue<string>("CosmosDbDatabaseId"), 
+                                              containerId: _config.GetValue<string>("CosmosDbContainerId"));
+        }
+
+
+    }
+}
